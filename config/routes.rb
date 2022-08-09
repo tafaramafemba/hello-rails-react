be10be1d0e28e66644e00b7a9f8b6cfe190077bc
tafaramafemba <tafara.maf@gmail.com>
2022-08-09 10:48:51 +0200

    implement random greeting generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "greetings#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
+  get "*path", to: "greetings#index", via: :all
 end
